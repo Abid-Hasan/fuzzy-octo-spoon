2e99c690bae4b6aa502240bad62543ae79c41bd3
Abid-Hasan <ahsnigdho@gmail.com>
2021-08-26 12:36:55 +0600

    First one done

--- assignment.js
+++ assignment.js
@@ -1,1 +1,9 @@
-console.log(2);
+function seerToMon(seer) {
+    if (typeof seer != "number") return "Please provide a valid number.";
+    if (seer < 0) return "Quantity can not be negative.";
+
+    const mon = seer / 40;
+    return mon;
+}
+
+console.log(seerToMon(60));
