faf07d2fedd833cec0ad8706286c8aa275410384
Abid-Hasan <ahsnigdho@gmail.com>
2021-08-26 12:53:03 +0600

    second one done

--- assignment.js
+++ assignment.js
@@ -1,9 +1,33 @@
 function seerToMon(seer) {
-    if (typeof seer != "number") return "Please provide a valid number.";
-    if (seer < 0) return "Quantity can not be negative.";
+    // Input validation
+    if (typeof seer != "number") {
+        return "Please provide a valid number.";
+    }
+    if (seer < 0) {
+        return "Quantity can not be negative.";
+    }
 
+    // 40 seer = 1 mon
     const mon = seer / 40;
     return mon;
 }
 
-console.log(seerToMon(60));
+function totalSales(shirtQuantity, pantQuantity, shoeQuantity) {
+    // Input validation
+    if (typeof shirtQuantity != "number" || typeof pantQuantity != "number" || typeof shoeQuantity != "number") {
+        return "Please provide valid numbers.";
+    }
+    if (shirtQuantity < 0 || pantQuantity < 0 || shoeQuantity < 0) {
+        return "Quantity can not be negative.";
+    }
+
+    // Calculating individual sales
+    const shirtSales = shirtQuantity * 100;
+    const pantSales = pantQuantity * 200;
+    const shoeSales = shoeQuantity * 500;
+
+    // Returning total sales
+    return shirtSales + pantSales + shoeSales;
+}
+
+console.log(totalSales(1, 1, 1));
